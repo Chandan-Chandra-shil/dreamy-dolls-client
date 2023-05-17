5082dcf06bc920a838f8fbe3e94efd8c6001f154
Chandan-Chandra-shil <chandanchandrashil22@gmail.com>
2023-05-18 02:00:08 +0600

    add footer and login form

--- src/Shared/Login.jsx
+++ src/Shared/Login.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import login from "../assets/login.svg";
+import { Link } from "react-router-dom";
 const Login = () => {
   return (
-    <div>
-      <h1>login page</h1>
-      <Link to="/register">Register page</Link>
+    <div className=" container min-h-screen mx-auto  rounded-sm">
+      <div className="shadow mt-20 gap-4">
+        <form className="  ">
+          <div className="card-body  ">
+            <h1 className="text-3xl text-primary font-bold">Login</h1>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="text"
+                placeholder="password"
+                className="input input-bordered"
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </div>
+
+          <p className="text-center pb-8">
+            Dream Dolls New ? <Link to="/register" className="text-primary">Register</Link>
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
