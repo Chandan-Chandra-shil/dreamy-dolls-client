46f61766775a3d53d340bc14a8534279dba72a37
Chandan-Chandra-shil <chandanchandrashil22@gmail.com>
2023-05-18 15:33:19 +0600

    upload data server site using post method

--- src/Pages/AddAToys/AddAToys.jsx
+++ src/Pages/AddAToys/AddAToys.jsx
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { AuthContext } from "../../Providers/AuthProvider";
 
 const AddAToys = () => {
+  const {user} = useContext(AuthContext)
+  const handleAddToy = event => {
+    event.preventDefault()
+    const form = event.target;
+    const photo = form.photo.value;
+    const name = form.name.value;
+    const sellerName = user?.displayName;
+    const sellerEmail = user?.email
+    const category = form.category.value;
+    const price = form.price.value;
+    const rating = form.rating.value;
+    const description = form.description.value;
+  
+
+    const allToys = {
+      photo,
+      name,
+      sellerEmail,
+      sellerName,
+      category,
+      price,
+      rating,
+      description
+    }
+    console.log(allToys)
+    
+    fetch("http://localhost:5000/upload-toy", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(allToys),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+    
+
+  }
+
+
   return (
-    <div className="container mx-auto my-10">
-      <form>
+    <div className="container mx-auto mb-10 ">
+      <form onSubmit={handleAddToy} >
         <div className=" border shadow-md p-10 rounded-sm">
           <h1 className="text-4xl text-primary font-bold underline text-center">
             Add A Toy
@@ -11,7 +54,7 @@ const AddAToys = () => {
           <div className="card-body md:grid sm:grid-cols-1 md:grid-cols-2  gap-4 ">
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Toy Picture URL</span>
+                <span className="label-text text-lg ">Toy Picture URL</span>
               </label>
               <input
                 type="text"
@@ -21,33 +64,35 @@ const AddAToys = () => {
             </div>
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Name</span>
+                <span className="label-text text-lg">Name</span>
               </label>
               <input type="text" name="name" className="input input-bordered" />
             </div>
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Seller Name</span>
+                <span className="label-text text-lg">Seller Name</span>
               </label>
               <input
                 type="text"
                 name="sellerName"
+                defaultValue={user?.displayName}
                 className="input input-bordered"
               />
             </div>
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Seller Email</span>
+                <span className="label-text text-lg">Seller Email</span>
               </label>
               <input
                 type="email"
                 name="sellerEmail"
+                defaultValue={user?.email}
                 className="input input-bordered"
               />
             </div>
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Sub-category</span>
+                <span className="label-text text-lg">Sub-category</span>
               </label>
               <input
                 type="text"
@@ -57,17 +102,17 @@ const AddAToys = () => {
             </div>
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Price</span>
+                <span className="label-text text-lg">Price</span>
               </label>
               <input
                 type="text"
-                name="Price"
+                name="price"
                 className="input input-bordered"
               />
             </div>
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Rating</span>
+                <span className="label-text text-lg">Rating</span>
               </label>
               <input
                 type="text"
@@ -78,7 +123,7 @@ const AddAToys = () => {
 
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Available quantity</span>
+                <span className="label-text text-lg">Available quantity</span>
               </label>
               <input
                 type="text"
@@ -88,13 +133,12 @@ const AddAToys = () => {
             </div>
             <div className="form-control col-span-2">
               <label className="label">
-                <span className="label-text">Detail description</span>
+                <span className="label-text text-lg">Detail description</span>
               </label>
-              <input
-                type="text"
+              <textarea
                 name="description"
-                className="input input-bordered"
-              />
+                className="textarea textarea-bordered textarea-lg w-full"
+              ></textarea>
             </div>
           </div>
         </div>
