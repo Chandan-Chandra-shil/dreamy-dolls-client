3c275d4aba5b75bafecd32f197a6c19c4b5c3db4
Chandan-Chandra-shil <chandanchandrashil22@gmail.com>
2023-05-21 11:44:07 +0600

    add style about us page

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import about from "../../assets/about.jpg"
+import AOS from "aos";
+import "aos/dist/aos.css";
 const AboutUs = () => {
+  useEffect(() => {
+    AOS.init();
+  },[])
   return (
     <div className="container mx-auto ">
       <div className="grid md:grid-cols-3 items-center my-12 gap-10">
-        <div>
+        <div data-aos="fade-right">
           <img
             className=" w-full md:border-8 p-5 md:border-purple-500 "
             src={about}
             alt=""
           />
         </div>
-        <div className="md:col-span-2 space-y-6 ">
+        <div data-aos="fade-left" className="md:col-span-2 space-y-6 ">
           <h2 className="text-4xl px-5 text-purple-500 mb-5 font-mono font-bold">
             About US
           </h2>
@@ -34,12 +39,12 @@ const AboutUs = () => {
           </p>
           <p className="px-5">
             Join us on this enchanting journey of play and discovery. Whether
-            you are looking for a new friend for your little one or searching for
-            the perfect gift, we are here to help you find that dreamy doll that
-            will ignite their imagination and become a cherished companion for
-            years to come.
+            you are looking for a new friend for your little one or searching
+            for the perfect gift, we are here to help you find that dreamy doll
+            that will ignite their imagination and become a cherished companion
+            for years to come.
           </p>
-          
+
           <p className="px-5">
             Thank you for choosing Dreamy Dolls, where dreams come to life!
           </p>
